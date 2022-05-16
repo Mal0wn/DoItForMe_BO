844e62cd061b71b8a829f8c6529e032efed93431
Marine M <marine.michelot-cic@ibm.com>
2022-05-16 10:49:23 +0200

    refactor style header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import {  FiLogOut, FiUser, FiStopCircle } from "react-icons/fi";
 import {  RiAdvertisementLine } from "react-icons/ri";
 import { BiCog } from "react-icons/bi";
 import elon from '../../assets/elon.jpeg';
-import "./Header.css";
+import style from  "./Header.module.css";
 import {
     Link
 } from 'react-router-dom';
@@ -14,30 +14,30 @@ import {
 const Header = () => {
 
     return (
-            <div id="header">
-                <nav>
-                    <div className="logoText">
-                        <img className="imgUser" src={elon} alt={'image user'} />
+            <div  className={style.header}>
+                <nav className={style.headerNav}>
+                    <div className={style.logoText}>
+                        <img className={style.imgUser} src={elon} alt={'image user'} />
                         <p> User Name</p>
                     </div>
-                    <ul className="listLink">
+                    <ul className={style.listLink}>
                         <Link to="/users">
-                            <li className="navLeftItem" active="true" > <FiUser /> Utilisateurs</li>
+                            <li className={style.navLeftItem} active="true" > <FiUser /> Utilisateurs</li>
                         </Link>
                         <Link to="/missions">
-                            <li className="navLeftItem" ><FaArchive /> Annonces</li>
+                            <li className={style.navLeftItem} ><FaArchive /> Annonces</li>
                         </Link>
                         <Link to="/usersSignal">
-                            <li className="navLeftItem" ><RiAdvertisementLine /> Signalement utilisateurs</li>
+                            <li className={style.navLeftItem} ><RiAdvertisementLine /> Signalement utilisateurs</li>
                         </Link>
                         <Link to="/missionsSignal">
-                            <li className="navLeftItem"><FiStopCircle /> Signalements annonces</li>
+                            <li className={style.navLeftItem}><FiStopCircle /> Signalements annonces</li>
                         </Link>
                         <Link to="/settings">
-                            <li className="navLeftItem" ><BiCog /> Settings</li>
+                            <li className={style.navLeftItem} ><BiCog /> Settings</li>
                         </Link>
                     </ul>
-                    <button className="deco" ><FiLogOut /> Deconnexion</button>
+                    <button className={style.deco} ><FiLogOut /> Deconnexion</button>
                 </nav>
             </div>
 
