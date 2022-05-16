63c5f18e17ba63b1a0a4e0790cb8b208c233d6fe
Marine M <marine.michelot-cic@ibm.com>
2022-05-16 11:10:20 +0200

    refactor connexion style

--- src/pages/Connexion/Connexion.js
+++ src/pages/Connexion/Connexion.js
@@ -1,8 +1,9 @@
 import React from "react";
+import style from "./Connexion.module.css"
 
 export const Connexion = () => {
     return (
-        <div className='containConnexion'>
+        <div className={style.containConnexion}>
             <h1> Connexion page</h1>
         </div>
 
